d32d843ad0563c4e5e431fef158cf2d225cf36cf
Froylan Vargas <fvargas@foundationmedicine.com>
2022-06-30 18:23:41 -0500

    first commit

---
+++ HealthCheck/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HealthCheckComponent } from './health-check/health-check.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FetchDataComponent,
+    NavMenuComponent,
+    HealthCheckComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
